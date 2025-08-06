8a488c96c053355e448a225aa775e8fb69f3a89d
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-06 12:47:58 +0430

    l

--- first-react/src/Commponints/Form.jsx
+++ first-react/src/Commponints/Form.jsx
@@ -1,21 +1,21 @@
 export default function(){
     return(
-        <div>
+        <div className="w-[40%] mx-auto">
             <label htmlFor="Search">
-  <span className="text-sm font-medium text-gray-700"> Search </span>
+  <span className="text-xl font-medium text-gray-700"> Search </span>
 
   <div className="relative">
     <input
       type="text"
       id="Search"
-      className="mt-0.5 w-full rounded border-gray-300 pe-10 shadow-sm sm:text-sm"
+      className="mt-0.5 w-full rounded border-gray-300 pe-10 py-3.5 shadow-md sm:text-sm"
     />
 
     <span className="absolute inset-y-0 right-2 grid w-8 place-content-center">
       <button
         type="button"
         aria-label="Submit"
-        className="rounded-full p-1.5 text-gray-700 transition-colors hover:bg-gray-100"
+        className="rounded-full py-4 px-4 text-gray-700 transition-colors hover:bg-gray-100"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
